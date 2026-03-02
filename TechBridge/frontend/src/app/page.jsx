48bc40c98ad848944a310d1f577b90a708a70e01
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-03-02 11:02:46 -0300

    LOGIN

--- TechBridge/frontend/src/app/page.jsx
+++ TechBridge/frontend/src/app/page.jsx
@@ -1,3 +1,6 @@
+"Use client"
+
+
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button"
@@ -11,46 +14,93 @@ import {
 	FieldSet,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
+import { EyeOffIcon } from "lucide-react"
+
 
 export default function Home() {
+	// const [isVisible, setIsVisible] = useState(false)
+
 	return (
-		<div className="w-full max-w-md">
-			<form>
-				<FieldGroup className="border p-4 rounded-b-md">
-					<FieldSet>
-						<FieldLegend>Payment Method</FieldLegend>
-						<FieldGroup>
-							<Field>
-								<FieldLabel htmlFor="checkout-7j9-card-name-43j">
-									Email
-								</FieldLabel>
-								<Input
-									id="checkout-7j9-card-name-43j"
-									placeholder="Evil Rabbit"
-									required
-								/>
-							</Field>
-							<Field>
-								<FieldLabel htmlFor="checkout-7j9-card-number-uw1">
-									Senha
-								</FieldLabel>
-								<Input
-									id="checkout-7j9-card-number-uw1"
-									placeholder="1234 5678 9012 3456"
-									required
-								/>
-							</Field>
-						</FieldGroup>
-					</FieldSet>
+		<div className="w-vw h-svh flex justify-center content-center flex-wrap">
+			<div className="w-full max-w-md h-fit">
+				<form>
+					<FieldGroup className="border p-4 rounded-md">
+						<div className="w-full flex justify-center">
+							<Image
+								src="/TechBridge/TechBridgeText.svg"
+								width={240}
+								height={24}
+								alt="TechBridge"
+							/>
+						</div>
 
-					<FieldSeparator />
+						<FieldSeparator />
 
-					{/* Botão de Login */}
-					<Field orientation="horizontal">
-						<Button type="submit">Login</Button>
-					</Field>
-				</FieldGroup>
-			</form>
+						<FieldSet>
+							<FieldLegend variant="" className="text-2xl">Login</FieldLegend>
+
+							<FieldGroup>
+								<Field>
+									<FieldLabel htmlFor="checkout-7j9-card-name-43j">
+										Email
+									</FieldLabel>
+									<Input
+										id="emailLogin"
+										placeholder="E-mail"
+										required
+									/>
+								</Field>
+
+								<Field>
+									<FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+										Senha
+									</FieldLabel>
+									<Input
+										id="senhaLogin"
+										placeholder="Senha"
+										required
+									/>
+								</Field>
+
+								<Field className="max-w-sm">
+									<FieldLabel htmlFor="inline-end-input">Input</FieldLabel>
+									<InputGroup>
+										<InputGroupInput
+											id="inline-end-input"
+											type="password"
+											placeholder="Enter password"
+										/>
+										<InputGroupAddon align="inline-end">
+											<EyeOffIcon />
+										</InputGroupAddon>
+									</InputGroup>
+									<FieldDescription>Icon positioned at the end.</FieldDescription>
+								</Field>
+
+
+
+
+							</FieldGroup>
+						</FieldSet>
+
+						<FieldSeparator />
+
+						{/* Botão de Login */}
+						<Field orientation="horizontal" className="justify-center">
+							<Button type="submit" size="lg">Entrar</Button>
+						</Field>
+					</FieldGroup>
+				</form>
+			</div>
+
+			{/* <button onClick={document.documentElement.classList.toggle('dark')}>
+				Alternar tema
+			</button> */}
 		</div>
 	);
 }
