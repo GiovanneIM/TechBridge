80d6eb220076a26a4d58e636a72a7311647a2fe5
GABRIEL QUEIROZ MARQUES <25170172@educ123.sp.senai.br>
2026-03-09 11:17:43 -0300

    a

--- TechBridge/frontend/src/app/(main)/page.jsx
+++ TechBridge/frontend/src/app/(main)/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
 			{/* Hero Section */}
 			<div className="h-fit flex justify-center items-center gap-24 pt-8">
 
-				<div className="w-160">
+				<div className="w-160 flex flex-col gap-2">
 					<p className="font-genty text-4xl">Bem vindo à Tech<span className="text-techbridge">Bridge</span></p>
 
 					<p className="text-xl text-gray-500 font-semibold mb-2">
@@ -27,8 +27,8 @@ export default function Home() {
 					</p>
 
 					<div className="flex gap-3">
-						<Button className="bg-techbridge text-white cursor-pointer hover:animate-wiggle hover:text-background">Entrar em contato</Button>
-						<Button variant="outline" className="bg-secondary text-foreground cursor-pointer hover:bg-foreground hover:text-background hover:animate-wiggle">Conheça nosso serviço</Button>
+						<Button className="bg-techbridge text-white cursor-pointer hover:animate-wiggle hover:text-background font-bold">Entrar em contato</Button>
+						<Button variant="outline" className="bg-secondary text-foreground cursor-pointer hover:bg-foreground hover:text-background hover:animate-wiggle font-bold">Conheça nosso serviço</Button>
 					</div>
 				</div>
 
@@ -44,6 +44,18 @@ export default function Home() {
 
 			{/* Onda */}
 			<svg className="wave" viewBox="0 0 1440 320">
+				<path fill="#5170ff" fillOpacity="1"
+					d="M0,160 
+								C180,240 360,80 540,160 
+								C720,240 900,80 1080,160 
+								C1260,240 1440,160 1440,160 
+								L1440,320 L0,320 Z
+							"
+				>
+				</path>
+			</svg>
+
+			<svg className="wave rotate-180 -translate-y-1" viewBox="0 0 1440 250">
 				<path fill="#5170ff" fillOpacity="1"
 					d="M0,160 
 								C180,240 360,80 540,160 
