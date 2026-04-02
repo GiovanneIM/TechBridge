26865ac2ee2686cab2440414e225a07b45216e1c
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-04-02 10:41:03 -0300

    ads

---
+++ TechBridge/frontend/src/app/(home)/layout.jsx
@@ -0,0 +1,14 @@
+// LAYOUT SECONDÁRIO - COM HEADER
+
+import "../globals.css";
+
+import Header from "@/components/blocks/Header/page";
+import Footer from "@/components/blocks/Footer/page";
+
+export default function RootLayout({ children }) {
+	return (<>
+		<Header />
+		{children}
+		<Footer/>
+	</>);
+}
