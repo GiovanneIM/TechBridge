6b37245b2bb29bf38bd2e6b21e1b955a442cb444
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2026-04-14 19:33:54 +0000

    Layouts

--- TechBridge/frontend/src/app/(user)/(gerente)/layout.jsx
+++ TechBridge/frontend/src/app/(user)/(gerente)/layout.jsx
@@ -8,7 +8,7 @@ import CounteudoSidebar from "../../components/Sidebar/conteudoSidebar";
 
 
 
-export default function LayoutAdmin({ children }) {
+export default function LayoutGerente({ children }) {
     return (<>
         {/* Sidebar */}
         <Sidebar className="top-[61px] h-[calc(100vh-61px)] border-none" collapsible="icon">
@@ -23,26 +23,3 @@ export default function LayoutAdmin({ children }) {
         </main>
     </>);
 }
-
-
-
-
-
-// import { cookies } from 'next/headers'
-// import { redirect } from 'next/navigation'
-// import { jwtVerify } from 'jose'
-
-// const SECRET = new TextEncoder().encode(process.env.JWT_SECRET)
-
-// export default async function AdminLayout({ children }) {
-//   const token = cookies().get('token')?.value
-//   if (!token) redirect('/login')
-
-//   const { payload } = await jwtVerify(token, SECRET)
-
-//   if (payload.cargo !== 'admin') {
-//     redirect('/acesso-negado')
-//   }
-
-//   return <>{children}</>
-// }
--- TechBridge/frontend/src/app/(user)/layout.jsx
+++ TechBridge/frontend/src/app/(user)/layout.jsx
@@ -1,9 +1,7 @@
 // LAYOUT PARA PÁGINAS COM USUÁRIO LOGADO - Com header e sidebar
 
-import "../globals.css";
-
 import HeaderUser from "@/components/blocks/Header/HeaderUser";
-import Footer from "@/components/blocks/Footer/page";
+// import Footer from "@/components/blocks/Footer/page";
 
 import {
 	SidebarProvider,
@@ -21,21 +19,11 @@ export default function UserLayout({ children }) {
 				<SidebarProvider>
 					<div className="flex flex-col min-h-screen w-full">
 						{/* Header */}
-						<HeaderUser className="bg-purple-500"/>
+						<HeaderUser/>
 
+						{/* Sidebar e Conteúdo */}
 						<div className="flex flex-1">
-							{/* Sidebar */}
-							<Sidebar className="top-[61px] h-[calc(100vh-61px)] border-none" collapsible="icon">
-								<SidebarContent>
-									<CounteudoSidebar/>
-								</SidebarContent>
-							</Sidebar>
-
-							{/* Conteúdo */}
-							<main className=" flex-1 flex border-x">
-								{children}
-								{/* <Footer /> */}
-							</main>
+							{children}
 						</div>
 					</div>
 				</SidebarProvider>
