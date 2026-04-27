38cb92629be17b18ed525f3945dec93ba54a6bc4
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-04-27 09:35:33 -0300

    EmpresasRotas

--- TechBridge/backend/controllers/EmpresasController.js
+++ TechBridge/backend/controllers/EmpresasController.js
@@ -1,8 +1,14 @@
 import EmpresasModel from "../models/EmpresasModel.js";
 
 class EmpresasController {
-    static async registrarEmpresas(req, res) {}
-    static async atualizarEmpresas(req, res) {}
+    static async registrarEmpresas(req, res) {
+        const { empresa, gerente} = req.body;
+
+    }
+
+    static async atualizarEmpresas(req, res) {
+        const { }
+    }
 }
 
 export default EmpresasController;
