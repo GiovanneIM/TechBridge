655a413725238293e18462b59f577c77be4f64a7
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-04-27 11:22:21 -0300

    Validacoes

--- TechBridge/backend/controllers/EmpresasController.js
+++ TechBridge/backend/controllers/EmpresasController.js
@@ -1,13 +1,32 @@
 import EmpresasModel from "../models/EmpresasModel.js";
+import { validarNome } from "../utils/validacoes.js";
 
 class EmpresasController {
-    static async registrarEmpresas(req, res) {
-        const { empresa, gerente} = req.body;
 
+    // REGISTAR UMA NOVA EMPRESA
+    static async registrarEmpresas(req, res) {
+        const { empresa, gerente } = req.body;
+
+        try {
+            // VALIDADAR DADOS DA NOVA EMPRESA
+            const validacaoEmpresa = validarEmpresa(empresa)
+
+            // VALIDADAR DADOS DO NOVO GERENTE
+
+        }
+        catch {
+        }
     }
 
     static async atualizarEmpresas(req, res) {
-        const { }
+        const {
+            cnpj,
+            razao_social,
+            nome_fantasia,
+            endereco
+        } = req.body;
+
+
     }
 }
 
