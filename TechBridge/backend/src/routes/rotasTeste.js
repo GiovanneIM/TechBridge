b608e44d097f972ffea1c5116aa43334f90ba39a
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2026-05-13 19:11:25 +0000

    lll

--- TechBridge/backend/src/routes/rotasTeste.js
+++ TechBridge/backend/src/routes/rotasTeste.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { listarUsuarios } from '../controllers/teste.js';
+
 
 const router = express.Router();
 
-router.get('/supabase', listarUsuarios);
 
 
 export default router;
