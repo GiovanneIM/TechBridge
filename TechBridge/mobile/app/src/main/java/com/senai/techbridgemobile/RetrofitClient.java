b8722d2c87bdfb6fd7fec40894aba26db27954dd
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-05-19 11:06:08 -0300

    HOOK

--- TechBridge/mobile/app/src/main/java/com/senai/techbridgemobile/RetrofitClient.java
+++ TechBridge/mobile/app/src/main/java/com/senai/techbridgemobile/RetrofitClient.java
@@ -5,17 +5,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://10.84.7.6:3000/";
+    // URL DA API
+    private static final String BASE_URL = "http://10.84.7.6:3000";
 
+    // INSTÂNCIA DO BANCO DE DADOS
     private static Retrofit retrofit = null;
 
+    // GET - INSTÂNCIA DO BANCO
     public static Retrofit getRetrofitInstance() {
+        // SE A INSTÂNCIA NÃO EXISTIR
         if (retrofit == null) {
+            // CRIA A INSTÂNCIA
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
+
+        // RETORNA A INSTÂNCIA
         return retrofit;
     }
 }
