a18e2a15bec35282603a3508244d07e4471b0b0e
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-05-19 08:37:30 -0300

    build

--- TechBridge/mobile/app/src/main/java/com/senai/techbridgemobile/MainActivity.java
+++ TechBridge/mobile/app/src/main/java/com/senai/techbridgemobile/MainActivity.java
@@ -23,44 +23,52 @@ import com.senai.techbridgemobile.adapter.ChamadosAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton btnCarregar;
-    String token;
-
-    RecyclerView recyclerChamados;
-    ChamadosAdapter adapter;
+    FloatingActionButton btnRecarregar; // BOTÃO DE RECARREGAR
+    String token;                       // TOKEN DO USUÁRIO LOGADO
+    RecyclerView recyclerChamados;      // RECYCLER PARA LISTAGEM DE CHAMADOS
+    ChamadosAdapter adapter;            // ADAPTER PARA EXIBIR AS INFOS DOS CHAMADOS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCarregar = findViewById(R.id.btnCarregar);
+        // REFERÊNCIANDO O BOTÃO RECARREGAR
+        btnRecarregar = findViewById(R.id.btnRecarregar);
+        // AO CLICAR NO BOTÃO
+        btnRecarregar.setOnClickListener(v -> carregarChamados());
 
+        // OBTENDO O TOKEN DE SESSÃO
         token = getIntent().getStringExtra("token");
+
+        // REFERÊNCIANDO O RECYCLER
         recyclerChamados = findViewById(R.id.recyclerChamados);
         recyclerChamados.setLayoutManager(new LinearLayoutManager(this));
 
-        // 🔥 proteção contra token nulo
+        // CASO NÃO TENHA UM TOKEN
         if (token == null || token.isEmpty()) {
             Toast.makeText(this, "Token não encontrado", Toast.LENGTH_LONG).show();
-            return;
         }
-
-        btnCarregar.setOnClickListener(v -> carregarChamados());
     }
 
+    // FUNÇÃO PARA BUSCAR OS CHAMADOS NA API
     private void carregarChamados() {
-
+        // REFERÊNCIANDO A API
         ApiService api = RetrofitClient.getRetrofitInstance().create(ApiService.class);
 
+        // FAZENDO A CONSULTA À API
         api.carregarChamados("Bearer " + token).enqueue(new Callback<ChamadoResponse>() {
+
+            // AO RESPONDER
             @Override
             public void onResponse(Call<ChamadoResponse> call, Response<ChamadoResponse> response) {
 
+                // SUCESSO NA CONSULTA
                 if (response.isSuccessful() && response.body() != null) {
-
+                    // OBTENDO CORPO DA RESPOSTA
                     ChamadoResponse resposta = response.body();
 
+                    // DADOS DA RESPOSTA
                     if (resposta.getDados() == null || resposta.getDados().getChamados() == null) {
                         Toast.makeText(MainActivity.this,
                                 "Resposta vazia da API",
@@ -68,8 +76,10 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
 
+                    // CRIANDO LISTA DE CHAMADOS
                     List<Chamado> chamados = resposta.getDados().getChamados();
 
+                    // LISTA DE CHAMADOS VAZIA
                     if (chamados.isEmpty()) {
                         Toast.makeText(MainActivity.this,
                                 "Nenhum chamado encontrado",
@@ -77,12 +87,14 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
 
-                    List<Chamado> Chamados = resposta.getDados().getChamados();
-
+                    // CRIANDO ADAPTER DOS CHAMADOS
                     adapter = new ChamadosAdapter(chamados);
+                    // EXIBINDO OS CHAMADOS NO RECYCLER
                     recyclerChamados.setAdapter(adapter);
 
-                } else {
+                }
+                // ERRO NA CONSULTA
+                else {
                     Toast.makeText(MainActivity.this,
                             "Erro: " + response.code(),
                             Toast.LENGTH_LONG).show();
@@ -91,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            // FALHA AO CONSULTAR
             @Override
             public void onFailure(Call<ChamadoResponse> call, Throwable t) {
                 Toast.makeText(MainActivity.this,
