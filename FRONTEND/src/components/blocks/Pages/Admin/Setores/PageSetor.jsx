71aa71d6f5eac53855c22fe54d96e7a0eae4eb58
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2026-06-15 07:33:12 +0000

    as

--- FRONTEND/src/components/blocks/Pages/Admin/Setores/PageSetor.jsx
+++ FRONTEND/src/components/blocks/Pages/Admin/Setores/PageSetor.jsx
@@ -8,8 +8,12 @@ import { useHeader } from "@/context/HeaderContext";
 import LoadingPage from "../../../Holders/LoadingPage";
 import ErrorPage from "../../../Holders/ErrorPage";
 import { API_URL } from "@/lib/api";
-import { User2, Mail, Phone, Building2, BadgeCheck, icons } from "lucide-react";
-
+import { User2, Mail, Phone, Building2, BadgeCheck, icons, Calendar, Pencil, CheckCircle2, MinusCircle } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function PageMembro() {
     const { id: id_empresa, cod_setor } = useParams();
@@ -73,30 +77,140 @@ export default function PageMembro() {
     }
 
     // SETOR CARREGADO
-    else {
+    else if (setor) {
         content = (
-            <div className="p-6 space-y-6">
-                {/* HEADER CARD */}
-                <div className="bg-card border rounded-2xl p-6 flex items-center gap-6">
+            <div className="w-full p-4 flex-1 flex flex-col gap-4">
+                {/* CONTROLE */}
+                <Card className="
+                    w-full border p-4
+                    flex justify-between flex-col 2xl:flex-row
+                    gap-3
+                    overflow-hidden
+                ">
+                    <div className="flex flex-col sm:flex-row gap-3 font-semibold">
+                        <div className="rounded-xl border bg-muted/30 p-3 sm:w-50">
+                            <p className="text-sm text-muted-foreground">
+                                Status
+                            </p>
 
-                    <div className="w-20 h-20 rounded-full bg-muted flex items-center justify-center overflow-hidden border">
-                        <Icon className="text-muted-foreground" size={36} />
-                    </div>
-
-                    <div className="flex flex-col">
-                        <h1 className="text-xl font-bold">{setor?.nome}</h1>
-
-                        <div className="flex items-center gap-2 mt-1 text-sm text-muted-foreground">
-                            <BadgeCheck size={16} />
-                            {setor?.cod_setor}
+                            <div className="flex flex-wrap items-center gap-2 font-semibold">
+                                {setor.status
+                                    ? (<p className="text-green-700 dark:text-green-500">Ativa</p>)
+                                    : (<p className="text-red-700 dark:text-red-500">Inativa</p>)
+                                }
+                            </div>
                         </div>
 
-                        <div className="text-xs text-muted-foreground mt-1">
-                            {/* {membro?.empresa} */}
+                        <div className="rounded-xl border bg-muted/30 p-3 sm:w-50">
+                            <p className="text-sm text-muted-foreground">
+                                Setor registrado em
+                            </p>
+
+                            <p className="flex items-center gap-2 font-semibold break-words">
+                                <Calendar size={14} />
+                                <span>{format(new Date(setor.data_criacao), 'dd/MM/yyyy', { locale: ptBR })} </span>
+                            </p>
+                        </div>
+
+                        {!setor.status && (
+                            <div className="rounded-xl border bg-muted/30 p-3 sm:w-50">
+                                <p className="text-sm text-muted-foreground">
+                                    Setor desativada em
+                                </p>
+
+                                <p className="flex items-center gap-2 font-semibold break-words text-red-700 dark:text-red-500">
+                                    <Calendar size={14} />
+                                    <span>{format(new Date(setor.data_criacao), 'dd/MM/yyyy', { locale: ptBR })} </span>
+                                </p>
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="
+                        flex flex-wrap gap-3 items-center
+                        rounded-xl
+                    ">
+                        <Button className="button-background border w-full sm:w-50" asChild>
+                            <Link href={`/admin/empresas/${id_empresa}/setores/${cod_setor}/editar`}>
+                                <Pencil /> Editar setor
+                            </Link>
+                        </Button>
+
+                        {!setor.status
+                            ? (<Button className="
+                                    bg-green-700 hover:bg-green-600
+                                    dark:bg-green-600 dark:hover:bg-green-700
+                                    text-white
+                                    w-full sm:w-50"
+                            >
+                                <CheckCircle2 /> Reativar setor
+                            </Button>)
+                            : (<Button className="
+                                    bg-red-700 hover:bg-red-600 
+                                    dark:bg-red-600 dark:hover:bg-red-700
+                                    text-white
+                                    w-full sm:w-50"
+                            >
+                                <MinusCircle /> Desativar setor
+                            </Button>)
+                        }
+                    </div>
+                </Card>
+
+                <Card
+                    className="
+                        w-full border p-5
+                        flex flex-col
+                        gap-4
+                        overflow-hidden
+                    "
+                    style={{
+                        backgroundColor: `#${setor.cor_fundo}`,
+                        color: `#${setor.cor_texto}`,
+                    }}
+                >
+                    <div className="flex gap-4">
+                        {/* ICONE */}
+                        <div
+                            className="
+                            flex items-center justify-center
+                            w-16 h-16 rounded-xl
+                            bg-black/10
+                            shrink-0
+                        "
+                        >
+                            <Icon size={36} />
+                        </div>
+
+                        {/* NOME E CÓDIGO */}
+                        <div className="flex-1 min-w-0">
+                            <div className="flex flex-col gap-2 font-genty">
+                                <h1 className="text-2xl font-bold truncate">
+                                    {setor?.nome}
+                                </h1>
+
+                                <span
+                                    className="
+                                    text-xs font-medium
+                                    px-2 py-1 rounded-full
+                                    bg-black/10
+                                    w-fit
+                                "
+                                >
+                                    # {setor?.cod_setor}
+                                </span>
+                            </div>
                         </div>
                     </div>
 
-                </div>
+                    {/* <div>
+                        {setor?.descricao && (
+                            <p className="mt-2 text-md opacity-90 leading-relaxed">
+                                {setor.descricao}
+                            </p>
+                        )}
+                    </div> */}
+                </Card>
 
                 {/* DEBUG (opcional) */}
                 {/* <pre>{JSON.stringify(setor, null, 2)}</pre> */}
